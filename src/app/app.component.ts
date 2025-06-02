9080f60a8fcc0417569c63b9738ab617ad819c69
Kasu Samuel <kasusamuel17@gmail.com>
2025-06-02 09:50:40 +0000

    chore(app): update Angular to v20 and apply migrations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  standalone: true,
+  imports:[],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
