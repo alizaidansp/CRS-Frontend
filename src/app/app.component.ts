c5db7edb7ac7b85b7a8ad03a3f5bdc1019c3ba58
Kasu Samuel <kasusamuel17@gmail.com>
2025-06-02 08:13:25 +0000

    refactor(app): add changeDetection: ChangeDetectionStrategy.OnPush.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   standalone: true,
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  styleUrl: "./app.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   public title = "CRS-Frontend";
